e4152457019d1ec5a80c547a23c41753696cb555
hadramet <hadramet.sylla@outlook.fr>
2022-08-19 12:59:30 +0200

    add academic, teacher and admin authority handler

--- src/middleware/admin-authority.js
+++ src/middleware/admin-authority.js
@@ -32,6 +32,22 @@ function AdminAndAcademicPermission(req, next) {
     } else next()
 }
 
+function AdminAcademicAndTeacherPermission(req, next) {
+    if (!req.auth.uid) return next(new ScooError('Unauthorize user', 'request'))
+    if (req.auth.urole !== 'teacher') {
+        if (req.auth.urole !== 'academic') {
+            if (req.auth.urole !== 'admin')
+                next(
+                    new Error(
+                        'Only admin or academic user are authorize to proceed this request',
+                        'request'
+                    )
+                )
+            else next()
+        } else next()
+    } else next()
+}
+
 export function AdminPermissionHandler(req, res, next) {
     try {
         AdminPermission(req, next)
@@ -47,6 +63,18 @@ export function AdminPermissionHandler(req, res, next) {
 export function AdminAndAcademicPermissionHandler(req, res, next) {
     try {
         AdminAndAcademicPermission(req, next)
+    } catch (error) {
+        res.status(error.statusCode || 500).send({
+            success: false,
+            failed: 'request',
+            message: error.message || 'Internal server error',
+        })
+    }
+}
+
+export function AdminAcademicAndTeacherPermissionHandler(req, res, next) {
+    try {
+        AdminAcademicAndTeacherPermission(req, next)
     } catch (error) {
         res.status(error.statusCode || 500).send({
             success: false,
